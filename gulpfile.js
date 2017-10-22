0279365a53acfd0f5cf866a4a5344b5d799aeba9
Draichi <lucasdraichi@gmail.com>
2017-10-22 00:54:34 -0200

    gulpfile com cssmin e uglify

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp')
+  ,uglify = require('gulp-uglify')
+  ,cssmin = require('gulp-cssmin')
+  ,clean = require('gulp-clean');
+
+gulp.task('copy', ['clean'], function(){
+  return gulp.src('src/*')
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', function(){
+  return gulp.src('dist')
+  .pipe(clean());
+});
+
+gulp.task('build-js', function(){
+  return gulp.src('dist/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build-css', function(){
+  return gulp.src('dist/*.css')
+    .pipe(cssmin())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['copy'], function() {
+  gulp.start(['build-js', 'build-css']);
+});
