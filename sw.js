a337f26d090ea58645b54af807fd333532337d98
Lucas Draichi <lucasdraichi@gmail.com>
2018-03-23 17:32:33 -0300

    sw

--- sw.js
+++ sw.js
@@ -1,5 +1,16 @@
-self.addEventListener('install', function() {
+self.addEventListener('install', function(event) {
   console.log("sw installed");
+  event.waitUntil(
+    caches.open('static')
+      .then(function(cache) {
+        cache.addAll([
+          '/portfolio',
+          '/portfolio/dist/index.js',
+          '/portfolio/index.html',
+          '/portfolio/dist/style.css'
+        ]);
+      })
+  );
 });
 self.addEventListener('activate', function() {
   console.log("sw activate");
