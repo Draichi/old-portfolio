37a1d05dc4b7b1862ff93bab43ade9950c92753d
Lucas Draichi <lucasdraichi@gmail.com>
2018-03-23 18:40:17 -0300

    sww

--- sw.js
+++ sw.js
@@ -18,6 +18,6 @@ self.addEventListener('fetch', event => {
   event.respondWith(async function() {
     const cachedResponse = await caches.match(event.request);
     if (cachedResponse) return cachedResponse;
-    return fetch(event.request, {cache: "no-cache"});
+    return fetch(event.request, {cache: "default"});
   }());
 });
