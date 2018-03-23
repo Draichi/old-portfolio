f1239f049ffd444db36bd79db7e46ea446e32ad8
Lucas Draichi <lucasdraichi@gmail.com>
2018-03-23 18:15:37 -0300

    sww

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('static')
       .then(function(cache) {
-        cache.addAll([
+        return cache.addAll([
           '/portfolio',
           '/portfolio/dist/index.js',
           '/portfolio/dist/style.css'
@@ -16,13 +16,8 @@ self.addEventListener('activate', function() {
 });
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(res) {
-        if (res) {
-          return res;
-        } else {
-          return fetch(event.request, {credentials: 'include'});
-        }
-      })
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
   );
 });
