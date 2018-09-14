a9652ea963572c67df1193086db6e53e92e5d2dc
draichi <lucasdraichi@gmail.com>
2018-09-14 01:33:39 -0300

    ga and new code sample

--- src/index.js
+++ src/index.js
@@ -80,3 +80,13 @@ for (var i = 0; i < textHeader.length; ++i) {
   observer.observe(textHeader[i]);
 }
 /*end fade on scroll*/
+
+// <!-- Google Analytics -->
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-125805162-1', 'auto');
+ga('send', 'pageview');
+// <!-- End Google Analytics -->
